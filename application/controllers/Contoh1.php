a7a8075b213196473b3d8443e0d297fecbb6e546
vlaicuvskha <bimorzqllh@icloud.com>
2022-03-20 20:04:33 +0700

    Push Pertama

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo "Nama saya Bimo Rizqullah Sulistijo Saya mahasiswa Universitas Bina Sarana Informatika Ciputat dengan NIM 17190787";
+ }
+}
